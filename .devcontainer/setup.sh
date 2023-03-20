864a654ec3779b9cc8a17bcabf055e0730e32c40
ahaas207 <86922705+ahaas207@users.noreply.github.com>
2023-03-19 20:50:24 -0400

    Set Path for 'libxtst6' Package
    Removed the installation of libxtst6 package and instead set the LD_LIBRARY_PATH environment variable to include the directory containing the required library.

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,10 +3,6 @@
 # Update package index
 sudo apt-get update
 
-# Install required packages
-sudo apt-get install -y libxtst6
-
-
 # Install noVNC
 sudo apt-get update && sudo apt-get install -y novnc
 
@@ -15,6 +11,9 @@ su - $(whoami) -c "vncserver -geometry 1280x800"
 
 # Start noVNC
 nohup /usr/share/novnc/utils/launch.sh --vnc localhost:5901 &
+
+# Set LD_LIBRARY_PATH to include directory containing libXtst.so.6
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/workspaces/.codespaces/shared/editors/jetbrains/*/plugins/remote-dev-server/selfcontained/lib
 
 # Add port forwarding rule
 curl -sSL https://raw.githubusercontent.com/$GIT_USER/$GIT_REPO/main/codespace-port-forwarding.sh | sudo bash
