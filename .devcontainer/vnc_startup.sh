78d95ea4e834195b19f9c54bafb87d5a008eba05
ahaas207 <86922705+ahaas207@users.noreply.github.com>
2023-05-29 18:01:34 -0400

    Wait for X Server

--- .devcontainer/vnc_startup.sh
+++ .devcontainer/vnc_startup.sh
@@ -2,8 +2,12 @@
 # Start up OpenBox, our window manager
 openbox-session &
 
+# Wait for the X server to be ready
+while ! xdpyinfo >/dev/null 2>&1; do sleep 1; done
+
 # Start the VNC server with security disabled (for simplicity)
 x11vnc -display $DISPLAY -forever -shared -nopw -rfbport 5901 &
 
 # Start up NoVNC
 websockify --web /opt/noVNC/ 6901 localhost:5901
+
