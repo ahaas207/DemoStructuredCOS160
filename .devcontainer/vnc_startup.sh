56fd022080a6ac6cd5cab4dfdf7e9728fb8f04f0
ahaas207 <86922705+ahaas207@users.noreply.github.com>
2023-05-29 18:02:58 -0400

    Timeout if X server takes too long

--- .devcontainer/vnc_startup.sh
+++ .devcontainer/vnc_startup.sh
@@ -3,11 +3,20 @@
 openbox-session &
 
 # Wait for the X server to be ready
-while ! xdpyinfo >/dev/null 2>&1; do sleep 1; done
+timeout_counter=0
+while ! xdpyinfo >/dev/null 2>&1; do
+  sleep 1
+  timeout_counter=$((timeout_counter + 1))
+  if [ $timeout_counter -ge 60 ]; then
+    echo "Timeout waiting for X server to be ready"
+    exit 1
+  fi
+done
 
 # Start the VNC server with security disabled (for simplicity)
 x11vnc -display $DISPLAY -forever -shared -nopw -rfbport 5901 &
 
 # Start up NoVNC
 websockify --web /opt/noVNC/ 6901 localhost:5901
+
 
