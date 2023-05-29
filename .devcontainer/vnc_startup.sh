13e05d0c8673b026f1a6c3543d7d6c466b22de29
ahaas207 <86922705+ahaas207@users.noreply.github.com>
2023-05-29 13:59:01 -0400

    Update vnc_startup.sh with correct port

--- .devcontainer/vnc_startup.sh
+++ .devcontainer/vnc_startup.sh
@@ -3,7 +3,7 @@
 openbox-session &
 
 # Start the VNC server with security disabled (for simplicity)
-x11vnc -display $DISPLAY -forever -shared -nopw &
+x11vnc -display $DISPLAY -forever -shared -nopw -rfbport 5901 &
 
 # Start up NoVNC
-websockify --web /opt/noVNC/ $NO_VNC_PORT localhost:$VNC_PORT
+websockify --web /opt/noVNC/ 6901 localhost:5901
